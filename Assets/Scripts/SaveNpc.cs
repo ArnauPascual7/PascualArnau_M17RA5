966494350ad6687d44a8d380911c2ca8c553332d
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2026-02-04 12:20:07 +0100

    Save Npc Additions and Fix

--- Assets/Scripts/SaveNpc.cs
+++ Assets/Scripts/SaveNpc.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,6 +8,12 @@ public class SaveNpc : MonoBehaviour, IInteractable
     [SerializeField] private string _prompt = "Prem \"E\" per interactuar";
     [SerializeField] private GameObject _interactionCanvas;
 
+    [Header("Text Messages")]
+    [SerializeField] private TextMeshProUGUI _uiText;
+    [SerializeField] private string _msgSave = "El joc s'ha guardat";
+    [SerializeField] private string _msgLoad = "El joc s'ha carregat";
+    [SerializeField] private string _msgEquip = "S'ha canviat l'estat del objecte equipat";
+
     public string InteractionPrompt => _prompt;
 
     [Header("Interact Events")]
@@ -15,6 +22,8 @@ public class SaveNpc : MonoBehaviour, IInteractable
 
     public bool Interact(PlayerInteraction player)
     {
+        _uiText.text = string.Empty;
+
         OnInteract.Invoke();
         _interactionCanvas.SetActive(true);
 
@@ -23,16 +32,19 @@ public class SaveNpc : MonoBehaviour, IInteractable
 
     public void SaveGame()
     {
+        _uiText.text = _msgSave;
         SaveSystem.Save();
     }
 
     public void LoadGame()
     {
+        _uiText.text = _msgLoad;
         SaveSystem.Load();
     }
 
     public void EquipDesequipObj()
     {
+        _uiText.text = _msgEquip;
         PlayerController.Instance.objCollected = !PlayerController.Instance.objCollected;
         PlayerController.Instance.EquipPartyHat(PlayerController.Instance.objCollected);
     }
